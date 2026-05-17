72d0633667622e5ed9cb0c1c199cf298e2329d70
FadhilahAfif <fadhilahafif257@gmail.com>
2026-05-17 16:10:03 +0700

    feat: Add new vector drawables for Berima logo and update launcher icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         applicationId = "upnvj.berima.v1"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
